61dfa95013faa1c398170d44bf413490d3638d3a
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-15 16:23:41 +0200

    feat: addnconmtainer

---
+++ src/components/AppMain.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: 'AppMain',
+}
+</script>
+
+<template>
+    <section class="cards-list">
+        <div class="container">
+            <div class="yugi-cards">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi explicabo, dolore eveniet dicta fugit inventore asperiores placeat veniam nihil! Voluptas corporis totam, recusandae aspernatur dicta facilis veniam ad eos repellat.
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+.cards-list {
+    background-color: #d48f38;
+    padding-top: 24px ;
+    padding-bottom: 24px;
+}
+.container {
+    width: 80%;
+    margin: 0 auto;
+    background-color: #fff;
+    .yugi-cards {
+
+    }
+}
+</style>
