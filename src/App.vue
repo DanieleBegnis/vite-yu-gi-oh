09cf27cfdd5809878a11d7211b5c552ff67330b2
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-16 17:59:58 +0200

    feat add search bar

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,58 @@ import axios from 'axios';
 import { store } from './store.js';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import CardSearcher from './components/CardSearcher.vue';
 
 export default {
   components: {
     AppHeader,
-    AppMain
+    AppMain,
+    CardSearcher
   },
   data() {
     return {
       store
+      
     };
   },
   methods: {
     getCardsFromApi() {
-      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+      let apiUrl = 'https://db.ygoprodeck.com/api/v7/cardinfo.php';
+      const queryParams = {
+        num: 20,
+        offset: 0
+      };
+      if(store.searchedStatus !== '') {
+        queryParams.archetype = store.searchedStatus;
+      }
+      axios.get(apiUrl, {
+        params: queryParams
+      })
       .then((response) => {
-        console.log(response.data);
         store.cards = response.data.data;
       });
-
+    },
+    getArchetypeFromApi() {
+      axios.get('https://db.ygoprodeck.com/api/v7/archetypes.php')
+      .then((response) => {
+        console.log(response.data);
+        store.archetype = response.data;
+      });
     }
   },
   mounted() {
     this.getCardsFromApi();
+    this.getArchetypeFromApi();
   }
 }
+
 </script>
+
 
 
 <template>
   <AppHeader></AppHeader>
+  <CardSearcher @searchPerfomed="getCardsFromApi"></CardSearcher>
   <AppMain></AppMain>
 </template>
 
---
+++ src/components/CardSearcher.vue
@@ -0,0 +1,41 @@
+<script>
+import { store } from '../store.js';
+
+export default {
+    name: 'CardSearcher',
+    data() {
+        return {
+            store,
+        };
+    }
+}
+</script>
+
+<template>
+    <section class="app-search">
+        <div class="container">
+            <select @change="$emit('searchPerformed')" v-model="store.searchedStatus">
+                <option value="">Scegli l'Archetipo</option>
+                <option v-for="archetype in store.archetype" :value="archetype">{{ archetype.archetype_name }}</option>
+            </select>
+        </div>
+    </section>
+</template>
+
+
+
+<style scoped lang="scss">
+    .app-search {
+        background-color: #d48f38
+    }
+    .container {
+    width: 80%;
+    margin: 0 auto;
+    select {
+        margin: 20px 0;
+        padding: 8px;
+        font-size: 12px;
+    }
+    }
+
+</style>
